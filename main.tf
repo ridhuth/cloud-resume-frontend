0cd7d6bdf91e9c97f1e436e7d86841b7a0db1f87
riddy92 <32391487+riddy92@users.noreply.github.com>
2023-10-17 21:29:18 +0000

    worplace select

--- main.tf
+++ main.tf
@@ -1,15 +1,23 @@
 
 
 module "s3_bucket" {
-
+  count      = terraform.workspace == "default" ? 1 : 0
   source         = "./modules/s3-website-buckets"
   s3_redirect_name         = "rhresume.com"
   s3_web_name              = "www.rhresume.com"
 
 }
 
-module "distributions" {
+module "s3_bucket_dev" {
+  count      = terraform.workspace == "dev" ? 1 : 0
+  source         = "./modules/s3-website-buckets"
+  s3_redirect_name         = "rhresume.com-${terraform.workspace}"
+  s3_web_name              = "www.rhresume.com-${terraform.workspace}"
 
+}
+
+module "distributions" {
+  count      = terraform.workspace == "default" ? 1 : 0
   source         = "./modules/distributions"
   index_html_etag = module.s3_bucket.index_html_etag
   counter_js_etag = module.s3_bucket.counter_js_etag
@@ -18,7 +26,7 @@ module "distributions" {
 }
 
 module "actions_role" {
-
+  count      = terraform.workspace == "default" ? 1 : 0
   source         = "./modules/iam_resources"
 
 }
