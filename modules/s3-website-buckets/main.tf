0d885a90b6e2768db0f8a1e1fd95159a6947cfbb
riddy92 <32391487+riddy92@users.noreply.github.com>
2023-10-20 19:10:51 +0000

    distributions-dev

--- modules/s3-website-buckets/main.tf
+++ modules/s3-website-buckets/main.tf
@@ -31,10 +31,20 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "rhresume_bucket_e
   }
 }
 
+/*
 resource "aws_s3_bucket_website_configuration" "s3_redirect_domain" {
   bucket                = "rhresume.com"
   redirect_all_requests_to {
     host_name = "www.rhresume.com"
+    protocol  = "https"
+  }
+}
+*/
+
+resource "aws_s3_bucket_website_configuration" "s3_redirect_domain" {
+  bucket                = var.s3_redirect_name
+  redirect_all_requests_to {
+    host_name = var.s3_web_name
     protocol  = "https"
   }
 }
