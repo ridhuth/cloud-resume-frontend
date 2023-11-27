8ca3e4674f690c92ac65b6f8fa83ae423b95327c
riddy92 <32391487+riddy92@users.noreply.github.com>
2023-11-27 02:04:11 +0000

    applying resources foreach pull

--- modules/s3-website-buckets/main.tf
+++ modules/s3-website-buckets/main.tf
@@ -9,10 +9,9 @@ resource "aws_s3_bucket" "rhresume_bucket" {
 
 
 
-
-
 resource "aws_s3_bucket_versioning" "rhresume_bucket_versioning" {
-  bucket                = var.s3_redirect_name
+  # bucket                = var.s3_redirect_name
+  bucket                = aws_s3_bucket.rhresume_bucket.id
   expected_bucket_owner = null
   mfa                   = null
   versioning_configuration {
@@ -23,7 +22,7 @@ resource "aws_s3_bucket_versioning" "rhresume_bucket_versioning" {
 
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "rhresume_bucket_encryption" {
-  bucket                = var.s3_redirect_name
+  bucket                = aws_s3_bucket.rhresume_bucket.id
   expected_bucket_owner = null
   rule {
     bucket_key_enabled = true
@@ -47,7 +46,7 @@ resource "aws_s3_bucket" "wwwrhresume_bucket" {
 
 
 resource "aws_s3_bucket_versioning" "wwwrhresume_bucket_versioning" {
-  bucket                = var.s3_web_name
+  bucket                = aws_s3_bucket.wwwrhresume_bucket.id
   expected_bucket_owner = null
   mfa                   = null
   versioning_configuration {
@@ -58,7 +57,7 @@ resource "aws_s3_bucket_versioning" "wwwrhresume_bucket_versioning" {
 
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "wwwrhresume_bucket_encryption" {
-  bucket                = var.s3_web_name
+  bucket                = aws_s3_bucket.wwwrhresume_bucket.id
   expected_bucket_owner = null
   rule {
     bucket_key_enabled = true
