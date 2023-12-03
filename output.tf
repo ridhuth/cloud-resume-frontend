5fc97893b5dd20dc662a818733df58bb61410d73
riddy92 <32391487+riddy92@users.noreply.github.com>
2023-12-03 02:26:04 +0000

    adding output

--- output.tf
+++ output.tf
@@ -12,19 +12,20 @@ output "redirect_distribution_domain" {
 output "redirect_distribution_domain" {
 
     value = {
-      domain_name = for k in module.distributions_dev : k.redirect_distribution_domain
-      origin = for k in module.s3_bucket_dev : k.s3_redirect_bucket_name
+        domain_name = [ for k in module.distributions_dev : k.redirect_distribution_domain ]
+        origin = [ for k in module.s3_bucket_dev : k.s3_redirect_bucket_name ] 
     }
+    
     description = "Domain name corresponding to the distribution and bucket name so appropriate test environment index is known"
-  
+
 }
 
 
 output "distribution_domain" {
 
     value = {
-      domain_name = for k in module.distributions_dev : k.distribution_domain
-      origin = for k in module.s3_bucket_dev : k.s3_bucket_name
+      domain_name = [ for k in module.distributions_dev : k.distribution_domain ]
+      origin = [ for k in module.s3_bucket_dev : k.s3_bucket_name ]
     }
     description = "Domain name corresponding to the distribution and bucket name so appropriate test environment index is known"
   
