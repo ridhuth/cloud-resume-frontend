07451572bb5f3b6e7d59f90f38829e7bf8fc8873
riddy92 <32391487+riddy92@users.noreply.github.com>
2023-12-03 00:24:56 +0000

    adding output

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "redirect_distribution_domain" {
-  value       = module.distributions_dev[each.key].redirect_distribution_domain
+  value       = module.distributions_dev.redirect_distribution_domain
   description = "Domain name corresponding to the distribution. For example: d604721fxaaqy9.cloudfront.net"
 }
 
 output "distribution_domain" {
-  value       = module.distributions_dev[each.key].distribution_domain
+  value       = module.distributions_dev.distribution_domain
   description = "Domain name corresponding to the distribution. For example: d604721fxaaqy9.cloudfront.net"
 }
