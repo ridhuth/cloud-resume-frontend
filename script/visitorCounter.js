1da95e1d1b612f0cf204f30ca9f88c48681c38a0
riddy92 <32391487+riddy92@users.noreply.github.com>
2024-10-05 03:40:59 +0000

    html and jscript changes

--- script/visitorCounter.js
+++ script/visitorCounter.js
@@ -1,6 +1,11 @@
+function counter() {
+
 fetch('https://y6v9mkpq3b.execute-api.us-east-1.amazonaws.com/default/updateDynamoDB')
 .then(response => response.json()) 
 .then(data => {
     console.log(data.Attributes["count"]["N"]);
     document.getElementById("viewCount").innerHTML = data.Attributes["count"]["N"];
  })
+}
+
+document.addEventListener("DOMContentLoaded",counter);
