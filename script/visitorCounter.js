7e46ae2f6de3a0057c7fcdec4d74163cb45ccf85
riddy92 <32391487+riddy92@users.noreply.github.com>
2024-10-06 05:11:56 +0000

    javascript changes

--- script/visitorCounter.js
+++ script/visitorCounter.js
@@ -1,4 +1,17 @@
 
+function counter() {
+
+    fetch('https://y6v9mkpq3b.execute-api.us-east-1.amazonaws.com/default/updateDynamoDB')
+    .then(response => response.json()) 
+    .then(data => {
+        console.log(data.Attributes["count"]["N"]);
+        document.getElementById("viewCount").innerHTML = data.Attributes["count"]["N"];
+        return data;
+     })
+    }
+
+
+
 // See if we have a viewed value
 if (sessionStorage.getItem("viewed") === null) {
     let counter = counter();
@@ -11,16 +24,7 @@ else {
     document.getElementById("viewCount").innerHTML = sessionStorage.getItem("count")
 }
 
-function counter() {
 
-fetch('https://y6v9mkpq3b.execute-api.us-east-1.amazonaws.com/default/updateDynamoDB')
-.then(response => response.json()) 
-.then(data => {
-    console.log(data.Attributes["count"]["N"]);
-    document.getElementById("viewCount").innerHTML = data.Attributes["count"]["N"];
-    return data;
- })
-}
 
 // document.addEventListener("DOMContentLoaded",counter);
 
